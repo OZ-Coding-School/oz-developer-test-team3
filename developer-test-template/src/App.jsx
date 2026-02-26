e7c55a7fc41a9412db66e612c035453100f65e8b
Clacki <skthk6613@naver.com>
2026-02-26 16:23:58 +0900

    feat: 라우터 설정 (#18)

--- developer-test-template/src/App.jsx
+++ developer-test-template/src/App.jsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import TestPage from './pages/TestPage';
+import QuestionPage from './pages/QuestionPage';
+import ResultPage from './pages/ResultPage';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/test" element={<TestPage />}></Route>
+      <Route path="/question" element={<QuestionPage />}></Route>
+      <Route path="/result" element={<ResultPage />}></Route>
+      <Route path="*" element={<div>404 잘못된 페이지 요청입니다</div>} />
+    </Routes>
   );
 }
 
