581098bdb0339fad3c5c019aeb1fdc764a7d441b
0rrriiinnn0 <0rrriiinnn0@naver.com>
2026-02-26 15:04:47 +0900

    feat: 카드 컴포넌트 구현  및 배지 정렬

--- developer-test-template/src/App.jsx
+++ developer-test-template/src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
 import HomePage from './pages/HomePage';
-import TestPage from './pages/TestPage';
 
 function App() {
   return (
     <>
       <HomePage />
-      <TestPage />
     </>
   );
 }
